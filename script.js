7c9d50d6d77649d2dc4a694d2732327d4296419c
Razvan <78686556+Asura83@users.noreply.github.com>
2022-01-28 10:57:25 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
+function get_current_year(){
+    var now =new Date();
+    var current_year=now.getFullYear();
+    return current_year;
+};
+function calcule(amount= 0, rate=0, no_years=0){
+    
+    var a = (rate*amount)/100;
+    var res = no_years * a;
+return res;
+}
 function compute()
 {
-    var principal = document.getElementById("principal").value;
-    var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value;
-    var interest = principal * years * rate /100;
-    var year = new Date().getFullYear()+parseInt(years);
+    
+//    var amount = document.querySelector('input[name="amount"]').value;
+   var rate = document.querySelector('input[name="rate"]').value;
+//    var no_years = document.querySelector('input[name="no_years"]').value;
 
+    // var result = calcule(amount,rate,no_years);
+    alert(rate);
+    
 }
-function updateRate() 
-{
-    var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
-    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
-}
-window.onload = function () {
-    var slider = document.getElementById("slider");
-    slider.addEventListener("input", function () {
-        document.body.style.setProperty("--thumbNumber", "'" + this.value + "'");
-<span id="valBox"></span>
-<input type="range" min="5" max="10" step="1" 
-   oninput="showVal(this.value)" onchange="showVal(this.value)">
+    
